69b088db4d0e849cf0f28e55c3ea56016e307a19
Isabela <55512909+belasouza@users.noreply.github.com>
2024-06-17 12:07:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const url = "https://public.tableau.com/views/ORCIDCollaborationsVisualization/F
 const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
-    height: 1200,
+    height: 1500,
     width: 1200,
     onFirstInteraction: function() {
         workbook = viz.getWorkbook();
